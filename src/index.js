d9d338dc2306dc471a1f290b68520f61418a5b24
zhanglei <13472276896@163.com>
2019-07-26 18:58:30 +0800

    wqerw

--- src/index.js
+++ src/index.js
@@ -11,7 +11,6 @@ Sentry.init({
   release: 'hello-react@1.0.1',
 });
 
-
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
