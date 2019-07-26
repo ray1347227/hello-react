14c83579047291636c1f9601e8b4ee246a4358e6
zhanglei <13472276896@163.com>
2019-07-26 10:01:47 +0800

    test

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import Hook from './components/Hook';
 import './App.css';
 
 class App extends Component {
+  testSentry = () => {
+    this.asdfghjkl();
+  }
   render() {
     return (
       <div className="App" style={{ textAlign: 'center' }}>
         <Hook />
         <button
+          onClick={this.testSentry}
           style={{ padding: '2px 5px', background: '#abcdef', marginTop: 50 }}
         >
           sentry test
